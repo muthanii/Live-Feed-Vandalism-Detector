f040d878376a578cd00500e3d35783dc40a1f568
muthanii <muthanalsaadi@gmail.com>
2025-03-10 11:00:59 +0300

    Clean up whitespace and formatting in interface.py for improved consistency

--- src/streamlit/interface.py
+++ src/streamlit/interface.py
@@ -1,20 +1,22 @@
 from streamlit import st
 from yolov8.model import load_model, perform_inference
 
+
 def main():
     st.title("YOLOv8 Streamlit App")
-    
+
     uploaded_file = st.file_uploader("Upload an image", type=["jpg", "jpeg", "png"])
-    
+
     if uploaded_file is not None:
         image = uploaded_file.read()
-        st.image(image, caption='Uploaded Image', use_column_width=True)
-        
+        st.image(image, caption="Uploaded Image", use_column_width=True)
+
         model = load_model()
         results = perform_inference(model, image)
-        
+
         st.subheader("Inference Results")
         st.write(results)
+
 
 if __name__ == "__main__":
     main()
